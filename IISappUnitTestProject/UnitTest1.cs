a4ca4db2e957f4714ca73fb8ee56bd8c5f98236e
jenkins013 <31985262+jenkins013@users.noreply.github.com>
2018-01-16 09:17:27 +0000

    update

--- IISappUnitTestProject/UnitTest1.cs
+++ IISappUnitTestProject/UnitTest1.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.IE;
-using System.Collections.ObjectModel;
+//using System.Collections.ObjectModel;
 using System.Configuration;
 using OpenQA.Selenium.Support.UI;
 using OpenQA.Selenium.Remote;
